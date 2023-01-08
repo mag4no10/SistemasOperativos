f896e91f2757921cd201cc1330cfed0d8f2f86ab
Jonay Faas Garcia <alu0101470948@ull.edu.es>
2023-01-08 20:02:41 +0000

    FIX: console exits with command status

--- c++/terminal/src/main.cc
+++ c++/terminal/src/main.cc
@@ -5,27 +5,27 @@ int main(int argc, char* argv[]) {
     while (true) {
         std::error_code error = print_prompt(last_command_status);
         if (error) {
-            return 1;
+            last_command_status = 1;
+            return last_command_status;
         }
         std::error_code error1 = read_line(STDIN_FILENO,linea);
         if (error1) {
-            continue;
+            last_command_status = 1;
+            return last_command_status;
         }
         if (linea.empty()) {
             continue;
         }
         std::vector<shell::command> list_of_commands = parse_line(linea);
         if (list_of_commands.empty()) {
-            continue;
+            last_command_status = 1;
+            return last_command_status;
         }
 
         auto [return_value, is_quit_requested] = execute_commands(list_of_commands);
         last_command_status = return_value;
-        if (is_quit_requested) {
-            break;
-        }
-        if (!isatty(STDIN_FILENO)) {
-            break;
+        if (is_quit_requested || !isatty(STDIN_FILENO)) {
+            return return_value;
         }
     }
     return 0;
